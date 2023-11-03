f0ff4cf6c42fc4082c7680d00d09c90bc19b12eb
ram111222 <81773298+ram111222@users.noreply.github.com>
2023-11-03 17:02:24 -0400

    Server.js is no longer needed

--- Server.js
+++ Server.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const server = express();
-server.all('/', (req, res)=>{
-    res.send('ayo I am ready!')
-})
-function keepAlive() {
-  server.listen(0, () => {console.log("Server is ready.")})
-}
-module.exports = keepAlive; 
+
